e097c3c808fcda6fa477d05c765f5ae3d80af9b2
Joris Cramwinckel <joris.cramwinckel@ortec-finance.com>
2021-07-08 11:50:52 +0200

    added conda env and empty python module

---
+++ ITR/data/__init__.py
@@ -0,0 +1,14 @@
+"""
+This module contains classes that create connections to data providers.
+"""
+
+from .data_provider import DataProvider
+from .csv import CSVProvider
+from .excel import ExcelProvider
+from .bloomberg import Bloomberg
+from .cdp import CDP
+from .iss import ISS
+from .trucost import Trucost
+from .urgentem import Urgentem
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ITR
 This Python module implements the ITR methodology.
+
+
+## Getting started for Contributors:
+
+```
+conda env create -f environment.yml
+conda activate itr_env
+```
