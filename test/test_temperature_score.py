b40a2916733b3c6067f28ebda964903492a2b87e
MichaelTiemannOSC <mtiemann@os-climate.org>
2022-01-01 00:58:18 +0000

    Add units to csv ingest and test cases

--- test/test_temperature_score.py
+++ test/test_temperature_score.py
@@ -7,6 +7,7 @@ from ITR.interfaces import ETimeFrames, EScope
 from ITR.temperature_score import TemperatureScore
 from ITR.portfolio_aggregation import PortfolioAggregationMethod
 
+from ITR.data.osc_units import ureg, Q_, PA_
 
 class TestTemperatureScore(unittest.TestCase):
     """
@@ -20,8 +21,16 @@ class TestTemperatureScore(unittest.TestCase):
         :return:
         """
         self.temperature_score = TemperatureScore(time_frames=[ETimeFrames.LONG], scopes=EScope.get_result_scopes())
-        self.data = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), "inputs",
+        df = pd.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), "inputs",
                                              "data_test_temperature_score.csv"), sep=";")
+        df['ghg_s1s2'] = df['ghg_s1s2'].astype('pint[MWh]')
+        df['ghg_s3'] = df['ghg_s3'].astype('pint[MWh]')
+        for cumulative in ['cumulative_budget', 'cumulative_target', 'cumulative_trajectory']:
+            df[cumulative] = df[cumulative].astype('pint[Mt CO2]')
+        df['benchmark_global_budget'] = df['benchmark_global_budget'].astype('pint[Gt CO2]')
+        df['benchmark_temperature'] = df['benchmark_temperature'].astype('pint[delta_degC]')
+        print(f"df = {df}")
+        self.data = df
 
     def test_temp_score(self) -> None:
         """
@@ -33,17 +42,17 @@ class TestTemperatureScore(unittest.TestCase):
         self.assertAlmostEqual(scores[
                                    (scores["company_name"] == "Company T") &
                                    (scores["scope"] == EScope.S1S2)
-                                   ]["temperature_score"].iloc[0], 1.82, places=2, msg="The temp score was incorrect")
+                                   ]["temperature_score"].iloc[0], Q_(1.82, ureg.delta_degC), places=2, msg="The temp score was incorrect")
         self.assertAlmostEqual(scores[
                                    (scores["company_name"] == "Company E") &
                                    (scores["scope"] == EScope.S1S2)
-                                   ]["temperature_score"].iloc[0], 1.84, places=2,
+                                   ]["temperature_score"].iloc[0], Q_(1.84, ureg.delta_degC), places=2,
                                msg="The fallback temp score was incorrect")
         self.assertAlmostEqual(scores[
                                    (scores["company_name"] == "Company AA") &
                                    (scores["time_frame"] == ETimeFrames.LONG) &
                                    (scores["scope"] == EScope.S1S2S3)
-                                   ]["temperature_score"].iloc[0], 1.79, places=5,
+                                   ]["temperature_score"].iloc[0], Q_(1.79, ureg.delta_degC), places=5,
                                msg="The aggregated fallback temp score was incorrect")
 
     def test_temp_score_overwrite_tcre(self) -> None:
@@ -53,48 +62,48 @@ class TestTemperatureScore(unittest.TestCase):
         :return:
         """
         overwritten_temp_score = self.temperature_score
-        overwritten_temp_score.c.CONTROLS_CONFIG.tcre = 1.0
+        overwritten_temp_score.c.CONTROLS_CONFIG.tcre = Q_(1.0, ureg.delta_degC)
         scores = overwritten_temp_score.calculate(self.data)
         self.assertAlmostEqual(scores[
                                    (scores["company_name"] == "Company T") &
                                    (scores["scope"] == EScope.S1S2)
-                                   ]["temperature_score"].iloc[0], 1.65, places=2, msg="The temp score was incorrect")
+                                   ]["temperature_score"].iloc[0], Q_(1.65, ureg.delta_degC), places=2, msg="The temp score was incorrect")
         self.assertAlmostEqual(scores[
                                    (scores["company_name"] == "Company E") &
                                    (scores["scope"] == EScope.S1S2)
-                                   ]["temperature_score"].iloc[0], 1.65, places=2,
+                                   ]["temperature_score"].iloc[0], Q_(1.65, ureg.delta_degC), places=2,
                                msg="The fallback temp score was incorrect")
         self.assertAlmostEqual(scores[
                                    (scores["company_name"] == "Company AA") &
                                    (scores["time_frame"] == ETimeFrames.LONG) &
                                    (scores["scope"] == EScope.S1S2S3)
-                                   ]["temperature_score"].iloc[0], 1.63, places=5,
+                                   ]["temperature_score"].iloc[0], Q_(1.63, ureg.delta_degC), places=5,
                                msg="The aggregated fallback temp score was incorrect")
 
     def test_portfolio_aggregations(self):
         scores = self.temperature_score.calculate(self.data)
         aggregations = self.temperature_score.aggregate_scores(scores)
-        self.assertAlmostEqual(aggregations.long.S1S2.all.score, 1.857, places=2,
+        self.assertAlmostEqual(aggregations.long.S1S2.all.score, Q_(1.857, ureg.delta_degC), places=2,
                                msg="Long WATS aggregation failed")
         self.temperature_score.aggregation_method = PortfolioAggregationMethod.TETS
         aggregations = self.temperature_score.aggregate_scores(scores)
-        self.assertAlmostEqual(aggregations.long.S1S2.all.score, 1.875, places=2,
+        self.assertAlmostEqual(aggregations.long.S1S2.all.score, Q_(1.875, ureg.delta_degC), places=2,
                                msg="Long TETS aggregation failed")
         self.temperature_score.aggregation_method = PortfolioAggregationMethod.MOTS
         aggregations = self.temperature_score.aggregate_scores(scores)
-        self.assertAlmostEqual(aggregations.long.S1S2.all.score, 1.869, places=2,
+        self.assertAlmostEqual(aggregations.long.S1S2.all.score, Q_(1.869, ureg.delta_degC), places=2,
                                msg="Long MOTS aggregation failed")
         self.temperature_score.aggregation_method = PortfolioAggregationMethod.EOTS
         aggregations = self.temperature_score.aggregate_scores(scores)
-        self.assertAlmostEqual(aggregations.long.S1S2.all.score, 1.840, places=2,
+        self.assertAlmostEqual(aggregations.long.S1S2.all.score, Q_(1.840, ureg.delta_degC), places=2,
                                msg="Long EOTS aggregation failed")
         self.temperature_score.aggregation_method = PortfolioAggregationMethod.ECOTS
         aggregations = self.temperature_score.aggregate_scores(scores)
-        self.assertAlmostEqual(aggregations.long.S1S2.all.score, 1.840, places=2,
+        self.assertAlmostEqual(aggregations.long.S1S2.all.score, Q_(1.840, ureg.delta_degC), places=2,
                                msg="Long ECOTS aggregation failed")
         self.temperature_score.aggregation_method = PortfolioAggregationMethod.AOTS
         aggregations = self.temperature_score.aggregate_scores(scores)
-        self.assertAlmostEqual(aggregations.long.S1S2.all.score, 1.869, places=2,
+        self.assertAlmostEqual(aggregations.long.S1S2.all.score, Q_(1.869, ureg.delta_degC), places=2,
                                msg="Long AOTS aggregation failed")
 
 
