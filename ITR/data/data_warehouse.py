29dcccbbe1ce44728f6428be2cfd39545b38fe0f
MichaelTiemannOSC <mtiemann@os-climate.org>
2022-01-03 08:54:57 +0000

    Accept PR suggestions.

--- ITR/data/data_warehouse.py
+++ ITR/data/data_warehouse.py
@@ -54,9 +54,7 @@ class DataWarehouse(ABC):
         company_info_at_base_year = self.company_data.get_company_intensity_and_production_at_base_year(company_ids)
         projected_production = self.benchmark_projected_production.get_company_projected_production(
             company_info_at_base_year)
-        # print(f"projected_production = {projected_production}")
 
-        # print(f"company_info_at_base_year = {company_info_at_base_year}")
         df_trajectory = self._get_cumulative_emission(
             projected_emission_intensity=self.company_data.get_company_projected_trajectories(company_ids),
             projected_production=projected_production).rename(self.column_config.CUMULATIVE_TRAJECTORY)
@@ -67,7 +65,6 @@ class DataWarehouse(ABC):
             projected_emission_intensity=self.benchmarks_projected_emission_intensity.get_SDA_intensity_benchmarks(
                 company_info_at_base_year),
             projected_production=projected_production).rename(self.column_config.CUMULATIVE_BUDGET)
-        # print(f"""\ndf_budget = {df_budget}\n\nf_budget.sum() = {df_budget.sum()}\n\n""")
         df_company_data = pd.concat([df_company_data, df_trajectory, df_target, df_budget], axis=1)
         df_company_data[self.column_config.BENCHMARK_GLOBAL_BUDGET] = pd.Series([self.benchmarks_projected_emission_intensity.benchmark_global_budget]*
                                                                                             len(df_company_data), dtype='pint[Gt CO2]',
@@ -118,10 +115,5 @@ class DataWarehouse(ABC):
         :param projected_production: PintArray of projected production amounts
         :return: cumulative emissions based on weighted sum of production
         """
-        # print(f"DW: projected_emission_intensity['US0185223007'] = {projected_emission_intensity.loc['US0185223007']}")
-        # print(f"DW: projected_production['US0185223007'] = {projected_production.loc['US0185223007']}")
-        # print(f"projected_emission_intensity = {projected_emission_intensity.iloc[1,0:5]}")
-        # print(f"projected_production = {projected_production.iloc[1,0:5]}")
         df = projected_emission_intensity.multiply(projected_production)
-        # print(f"post-mult = {df.iloc[1,0:5]}")
         return df.sum(axis=1).astype('pint[Mt CO2]')
