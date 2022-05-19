0a01b6c5388a59fabbf89cae463c24c547638a8e
David Kroon <35101727+dp90@users.noreply.github.com>
2022-05-19 17:31:12 +0200

    Move json comparison to function
    Signed-off-by: David Kroon <35101727+dp90@users.noreply.github.com>

--- test/test_projection.py
+++ test/test_projection.py
@@ -2,11 +2,48 @@ import json
 import unittest
 import os
 import datetime
+from typing import List
 import pandas as pd
 
 from utils import QuantityEncoder
 from ITR.data.base_providers import EITrajectoryProjector
 from ITR.interfaces import ICompanyData, ProjectionControls
+
+
+def is_pint_dict_equal(result: List[dict], reference: List[dict]) -> bool:
+    is_equal = True
+    for i in range(len(result)):
+        if json.dumps(result[i], cls=QuantityEncoder) != json.dumps(reference[i], cls=QuantityEncoder):
+            print(f"Differences in projections_dict[{i}]: company_name = {result[i]['company_name']}; company_id = {result[i]['company_id']}")
+            for k, v in result[i].items():
+                if k == 'ghg_s1s2' and not reference[i].get(k):
+                    print("ghg_s1s2")
+                    continue
+                if k == 'projected_intensities':
+                    for scope in result[i][k]:
+                        if reference[i][k].get(scope):
+                            vref = reference[i][k]
+                            if not v.get(scope):
+                                print(f"projection has no scope {scope} for projection_intensities")
+                                is_equal = False
+                            elif json.dumps(v[scope]['projections'], cls=QuantityEncoder) != json.dumps(vref[scope]['projections'], cls=QuantityEncoder):
+                                print(f"projected_intensities differ for scope {scope}")
+                                print(f"computed {k}:\n{json.dumps(v[scope]['projections'], cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref[scope]['projections'], cls=QuantityEncoder)}\n\n")
+                                is_equal = False
+                        elif v.get(scope):
+                            print(f"reference has no scope {scope} for projection_intensities")
+                            # ???? is_equal = False
+                    continue
+                try:
+                    vref = reference[i][k]
+                    if json.dumps(v, cls=QuantityEncoder) != json.dumps(vref, cls=QuantityEncoder):
+                        print(f"computed {k}:\n{json.dumps(v, cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref, cls=QuantityEncoder)}\n\n")
+                        is_equal = False
+                except KeyError as e:
+                    print(f"missing in reference: {k}: {json.dumps(v)}\n\n")
+                    is_equal = False
+
+    return is_equal
 
 
 class TestProjector(unittest.TestCase):
@@ -27,130 +64,34 @@ class TestProjector(unittest.TestCase):
         self.projector = EITrajectoryProjector()
 
     def test_project(self):
-        test_failed = False
         projections = self.projector.project_ei_trajectories(self.companies)
         with open(self.json_reference_path, 'r') as file:
             reference_projections = json.load(file)
 
         projections_dict = [projection.dict() for projection in projections]
+        test_successful = is_pint_dict_equal(projections_dict, reference_projections)
 
-        for i in range(len(projections_dict)):
-            if json.dumps(projections_dict[i], cls=QuantityEncoder) != json.dumps(reference_projections[i], cls=QuantityEncoder):
-                print(f"Differences in projections_dict[{i}]: company_name = {projections_dict[i]['company_name']}; company_id = {projections_dict[i]['company_id']}")
-                for k, v in projections_dict[i].items():
-                    if k == 'ghg_s1s2' and not reference_projections[i].get(k):
-                        print("ghg_s1s2")
-                        continue
-                    if k == 'projected_intensities':
-                        for scope in projections_dict[i][k]:
-                            if reference_projections[i][k].get(scope):
-                                vref = reference_projections[i][k]
-                                if not v.get(scope):
-                                    print(f"projection has no scope {scope} for projection_intensities")
-                                    test_failed = True
-                                elif json.dumps(v[scope]['projections'], cls=QuantityEncoder) != json.dumps(vref[scope]['projections'], cls=QuantityEncoder):
-                                    print(f"projected_intensities differ for scope {scope}")
-                                    print(f"computed {k}:\n{json.dumps(v[scope]['projections'], cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref[scope]['projections'], cls=QuantityEncoder)}\n\n")
-                                    test_failed = True
-                            elif v.get(scope):
-                                print(f"reference has no scope {scope} for projection_intensities")
-                                # ???? test_failed = True
-                        continue
-                    try:
-                        vref = reference_projections[i][k]
-                        if json.dumps(v, cls=QuantityEncoder) != json.dumps(vref, cls=QuantityEncoder):
-                            print(f"computed {k}:\n{json.dumps(v, cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref, cls=QuantityEncoder)}\n\n")
-                            test_failed = True
-                    except KeyError as e:
-                        print(f"missing in reference: {k}: {json.dumps(v)}\n\n")
-                        test_failed = True
-
-        self.assertEqual(test_failed, False)
-
+        self.assertEqual(test_successful, True)
 
     # Need test data in order to test mean
     def test_median(self):
-        test_failed = False
         projections = EITrajectoryProjector(ProjectionControls(TREND_CALC_METHOD=pd.DataFrame.median)).project_ei_trajectories(self.companies)
         with open(self.json_reference_path, 'r') as file:
             reference_projections = json.load(file)
 
         projections_dict = [projection.dict() for projection in projections]
 
-        for i in range(len(projections_dict)):
-            if json.dumps(projections_dict[i], cls=QuantityEncoder) != json.dumps(reference_projections[i], cls=QuantityEncoder):
-                print(f"Differences in projections_dict[{i}]: company_name = {projections_dict[i]['company_name']}; company_id = {projections_dict[i]['company_id']}")
-                for k, v in projections_dict[i].items():
-                    if k == 'ghg_s1s2' and not reference_projections[i].get(k):
-                        print("ghg_s1s2")
-                        continue
-                    if k == 'projected_intensities':
-                        for scope in projections_dict[i][k]:
-                            if reference_projections[i][k].get(scope):
-                                vref = reference_projections[i][k]
-                                if not v.get(scope):
-                                    print(f"projection has no scope {scope} for projection_intensities")
-                                    test_failed = True
-                                elif json.dumps(v[scope]['projections'], cls=QuantityEncoder) != json.dumps(vref[scope]['projections'], cls=QuantityEncoder):
-                                    print(f"projected_intensities differ for scope {scope}")
-                                    print(f"computed {k}:\n{json.dumps(v[scope]['projections'], cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref[scope]['projections'], cls=QuantityEncoder)}\n\n")
-                                    test_failed = True
-                            elif v.get(scope):
-                                print(f"reference has no scope {scope} for projection_intensities")
-                                # ???? test_failed = True
-                        continue
-                    try:
-                        vref = reference_projections[i][k]
-                        if json.dumps(v, cls=QuantityEncoder) != json.dumps(vref, cls=QuantityEncoder):
-                            print(f"computed {k}:\n{json.dumps(v, cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref, cls=QuantityEncoder)}\n\n")
-                            test_failed = True
-                    except KeyError as e:
-                        print(f"missing in reference: {k}: {json.dumps(v)}\n\n")
-                        test_failed = True
-
-        self.assertEqual(test_failed, False)
-
+        test_successful = is_pint_dict_equal(projections_dict, reference_projections)
+        self.assertEqual(test_successful, True)
 
     def test_upper_lower(self):
-        test_failed = False
         projections = EITrajectoryProjector(ProjectionControls(UPPER_PERCENTILE=0.9, LOWER_PERCENTILE=0.1)).project_ei_trajectories(self.companies)
         with open(self.json_reference_path, 'r') as file:
             reference_projections = json.load(file)
 
         projections_dict = [projection.dict() for projection in projections]
-
-        for i in range(len(projections_dict)):
-            if json.dumps(projections_dict[i], cls=QuantityEncoder) != json.dumps(reference_projections[i], cls=QuantityEncoder):
-                print(f"Differences in projections_dict[{i}]: company_name = {projections_dict[i]['company_name']}; company_id = {projections_dict[i]['company_id']}")
-                for k, v in projections_dict[i].items():
-                    if k == 'ghg_s1s2' and not reference_projections[i].get(k):
-                        print("ghg_s1s2")
-                        continue
-                    if k == 'projected_intensities':
-                        for scope in projections_dict[i][k]:
-                            if reference_projections[i][k].get(scope):
-                                vref = reference_projections[i][k]
-                                if not v.get(scope):
-                                    print(f"projection has no scope {scope} for projection_intensities")
-                                    test_failed = True
-                                elif json.dumps(v[scope]['projections'], cls=QuantityEncoder) != json.dumps(vref[scope]['projections'], cls=QuantityEncoder):
-                                    print(f"projected_intensities differ for scope {scope}")
-                                    print(f"computed {k}:\n{json.dumps(v[scope]['projections'], cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref[scope]['projections'], cls=QuantityEncoder)}\n\n")
-                                    test_failed = True
-                            elif v.get(scope):
-                                print(f"reference has no scope {scope} for projection_intensities")
-                                # ???? test_failed = True
-                        continue
-                    try:
-                        vref = reference_projections[i][k]
-                        if json.dumps(v, cls=QuantityEncoder) != json.dumps(vref, cls=QuantityEncoder):
-                            print(f"computed {k}:\n{json.dumps(v, cls=QuantityEncoder)}\n\nreference {k}:\n{json.dumps(vref, cls=QuantityEncoder)}\n\n")
-                            test_failed = True
-                    except KeyError as e:
-                        print(f"missing in reference: {k}: {json.dumps(v)}\n\n")
-                        test_failed = True
-
-        self.assertEqual(test_failed, False)
+        test_successful = is_pint_dict_equal(projections_dict, reference_projections)
+        self.assertEqual(test_successful, True)
 
 
 if __name__ == "__main__":
