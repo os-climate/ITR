30dc06e18ae24b07702fbec357e4e97c83f6c006
David Kroon <35101727+dp90@users.noreply.github.com>
2022-06-22 10:34:29 +0200

    Update required packages
    Signed-off-by: David Kroon <35101727+dp90@users.noreply.github.com>
    Signed-off-by: Michael Tiemann <72577720+MichaelTiemannOSC@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -13,43 +13,32 @@ setup(
     author='Ortec Finance',
     author_email='joris.cramwinckel@ortec-finance.com',
     packages=find_packages(),
-    download_url = "https://pypi.org/project/ITR-Temperature-Alignment-Tool/",
+    download_url="https://pypi.org/project/ITR-Temperature-Alignment-Tool/",
     url="https://github.com/os-climate/ITR",
     project_urls={
         "Bug Tracker": "https://github.com/os-climate/ITR",
         "Documentation": 'https://github.com/os-climate/ITR',
         "Source Code": "https://github.com/os-climate/ITR",
     },
-    keywords = ['Climate', 'ITR', 'Finance'],
+    keywords=['Climate', 'ITR', 'Finance'],
     package_data={
-        'SBTi': [],
+        'ITR': [],
     },
     include_package_data=True,
     install_requires=[
-                      # 'ca-certificates', # ==2021.10.8
-                      'certifi', # ==2021.10.8
-                      'et_xmlfile', # ==1.1.0
-                      'ipython', # ==8.1.1
-                      'jupyterlab', # ==3.3.0
-                      'matplotlib', # ==3.5.1
-                      'numpy==1.22.2',
-                      'openpyxl', # ==3.0.9
-                      'openscm-units', # ==0.5.0
-                      # 'openssl', # ==1.1.1l
-                      'pandas==1.4.1',
-                      'pint==0.18',
-                      'pint-pandas==0.2',
-                      'pip==22.0.3',
-                      'pydantic==1.8.2',
-                      # 'python==3.9',
-                      # 'python_abi==3.9',
-                      'pytz', # ==2021.3
-                      'setuptools', # ==60.9.3
-                      # 'sqlite', # ==3.37.0
-                      'wheel', # >=0.36.2
-                      'xlrd',
+                      'iam-units>=2021.11.12',
+                      'openpyxl>=3.0.9',
+                      'openscm-units>=0.5.0',
+                      'pandas>=1.4.2',
+                      'pint>=0.18',
+                      'pint-pandas>=0.2',
+                      'pip>=22.0.3',
+                      'pydantic>=1.8.2',
+                      'setuptools>=60.9.3',
+                      'wheel>=0.36.2',
+                      'xlrd>=2.0.1',
                       ],
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     extras_require={
         'dev': [
             'nose2',
@@ -65,7 +54,6 @@ setup(
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3 :: Only",
