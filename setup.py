9e9f5cef535845f2c3db8c299cf199eb5a13ba32
Michael Tiemann <72577720+MichaelTiemannOSC@users.noreply.github.com>
2023-08-07 20:33:39 -0400

    Remove Python 3.11 until we sort PyPi
    When we do sort it, we can add 3.11 back to all the relevant places.
    
    Signed-off-by: Michael Tiemann <72577720+MichaelTiemannOSC@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -74,7 +74,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development",
         "Topic :: Office/Business :: Financial",
